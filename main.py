4c40612a2a3a590985bc2c38e00d6b4453306376
100478705 <145397497+100478705@users.noreply.github.com>
2023-10-18 13:55:54 +0200

    encriptado de contraseñas

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from tkinter import Tk, Label, Button, Entry, Frame, END
+import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import pandas as pd
 import PySimpleGUI as sg
+
 
 #color de la ventana
 sg.theme('DarkRed')
@@ -8,6 +11,25 @@ sg.theme('DarkRed')
 
 EXCEL_FILE = './datos_cripto.xlsx'
 data_frame = pd.read_excel(EXCEL_FILE)
+
+
+for index, row in data_frame.iterrows():
+    salt = os.urandom(16)
+    contraseña = row['Contraseña']
+    contraseña_bytes = contraseña.encode('utf-8')
+    data_frame.at[index, 'Salt'] = salt
+    data_frame.to_excel('./datos_cripto.xlsx', index=False)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+        )
+    key = kdf.derive(contraseña_bytes)
+    data_frame.at[index, 'Key'] = kdf
+    data_frame.to_excel('./datos_cripto.xlsx', index=False)
+
+    
 
 #cada lista representa una fila en la pantalla de la app
 layout = [
@@ -18,8 +40,6 @@ layout = [
     [sg.Submit('Aceptar'), sg.Exit('Salir')]
 ]
 
-
-
 window = sg.Window('App Cripto', layout)
 
 #función para vaciar el input
@@ -29,13 +49,13 @@ def clear_input():
     return None
 
 while True:
-    event, values = window.read()
-    
+    event, values = window.read()   
     if event == sg.WIN_CLOSED or event == 'Salir':
         break
     if event == 'Aceptar':
         nickname = values['Nickname']
         contraseña = values['Contraseña']
+        contraseña_bytes = contraseña.encode('utf-8')
 
         #si los campos están vacíos lanzamos error
         if not nickname or not contraseña:
@@ -53,9 +73,5 @@ while True:
             if not exito:
                 sg.popup_error('Tus datos no coinciden con la base')           
             clear_input()   
-
-window.close()
-
-    
 
 window.close()
