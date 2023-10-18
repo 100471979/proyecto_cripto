4149cf5a3a599c123dda89d290d38d9dde041230
100471979 <100471979@alumnos.uc3m.es>
2023-10-18 13:43:35 +0200

    Encriptado de contraseñas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from tkinter import Tk, Label, Button, Entry, Frame, END
+import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import pandas as pd
 import PySimpleGUI as sg
 
@@ -8,6 +10,25 @@ sg.theme('DarkRed')
 
 EXCEL_FILE = './datos_cripto.xlsx'
 data_frame = pd.read_excel(EXCEL_FILE)
+
+
+for index, row in data_frame.iterrows():
+    salt = os.urandom(16)
+    contraseña = row['Contraseña']
+    contraseña_bytes = contraseña.encode('utf-8')
+    data_frame.at[index, 'Salt'] = salt
+    data_frame.to_excel('./datos_cripto.xlsx', index=False)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+        )
+    key = kdf.derive(contraseña_bytes)
+    data_frame.at[index, 'Key'] = kdf
+    data_frame.to_excel('./datos_cripto.xlsx', index=False)
+
+    
 
 #cada lista representa una fila en la pantalla de la app
 layout = [
@@ -27,13 +48,13 @@ def clear_input():
     return None
 
 while True:
-    event, values = window.read()
-    
+    event, values = window.read()   
     if event == sg.WIN_CLOSED or event == 'Salir':
         break
     if event == 'Aceptar':
         nickname = values['Nickname']
         contraseña = values['Contraseña']
+        contraseña_bytes = contraseña.encode('utf-8')
 
         #si los campos están vacíos lanzamos error
         if not nickname or not contraseña:
@@ -51,9 +72,5 @@ while True:
             if not exito:
                 sg.popup_error('Tus datos no coinciden con la base')           
             clear_input()   
-
-window.close()
-
-    
 
 window.close()
