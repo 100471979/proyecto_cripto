1f894d214a00e4d9b3c8bce039cb5ea74eaf0baf
100471979 <100471979@alumnos.uc3m.es>
2023-11-30 15:07:52 +0100

    certificados verificados

--- main.py
+++ main.py
@@ -9,278 +9,37 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.exceptions import InvalidSignature
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 import pandas as pd
 import PySimpleGUI as sg
 import datetime
 
-#firmar clave simetrica
-
 #color de la ventana
 sg.theme('DarkRed')
 
-EXCEL_FILE = './datos_cripto.xlsx'
-data_frame = pd.read_excel(EXCEL_FILE)
+data_frame = pd.read_excel("./datos_cripto.xlsx")
 data_frame2 = pd.read_excel("./Coordenadas.xlsx")
 data_frame3 = pd.read_excel("./Claves_privadas.xlsx")
 data_frame4 = pd.read_excel("./Autoridades.xlsx")
-
-#CODIGO PARA ENCRIPTAR LAS CONTRASEÑAS
-"""for index, row in data_frame.iterrows():
-    salt = os.urandom(16)
-    contraseña = row['Contraseña']
-    contraseña_bytes = contraseña.encode('utf-8')
-    data_frame.at[index, 'Salt'] = salt
-    data_frame.to_excel('./datos_cripto.xlsx', index=False)
-    kdf = Scrypt(
-        salt=salt,
-        length=32,
-        n=2**14,
-        r=8,
-        p=1,
-    )
-    key = kdf.derive(contraseña_bytes)
-    data_frame.at[index, 'Key'] = key
-    data_frame.to_excel('./datos_cripto.xlsx', index=False)"""
-
-#CODIGO PARA GENERAR LAS CLAVES PRIVADAS DE CADA USUARIO 
-"""for index, row in data_frame3.iterrows():
-    private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    )
-    pem = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
-    )
-    pem.splitlines()[0]
-    data_frame3.at[index, 'Privadas'] = pem
-    data_frame3.to_excel('./Claves_privadas.xlsx', index=False)"""
-
-#CODIGO PARA OBTENER LA CALVE PÚBLICA DE CADA USUARIO Y GUARDARLA EN LA BASE DE DATOS
-"""for index, row in data_frame.iterrows():
-    nickname1 = row['Nickname']
-    for index, row in data_frame3.iterrows():
-        nickname2 = row['Nickname']
-        privada = row['Privadas']
-        privada_pem = serialization.load_pem_private_key(ast.literal_eval(privada), password=None)
-        if nickname1 == nickname2:
-            public_key = privada_pem.public_key()
-            pem_public = public_key.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo
-            )
-            pem_public.splitlines()[0]
-            data_frame.at[index, 'Key_public'] = pem_public  
-            data_frame.to_excel('./datos_cripto.xlsx', index=False)"""
-
-#GENERACIÓN DE CLAVES PRIVADAS DE LOS CA
-"""for index, row in data_frame4.iterrows():
-    private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    )
-    pem = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
-    )
-    pem.splitlines()[0]
-    data_frame4.at[index, 'Privada'] = pem
-    data_frame4.to_excel('./Autoridades.xlsx', index=False)"""
-
-# GENERACIÓN DE CLAVE PÚBLICA DE LOS CA
-"""for index, row in data_frame4.iterrows():
-    nickname1 = row['Autoridad']       
-    privada = row['Privada']
-    privada_pem = serialization.load_pem_private_key(ast.literal_eval(privada), password=None)
-    
-    public_key = privada_pem.public_key()
-    pem_public = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    pem_public.splitlines()[0]
-    data_frame4.at[index, 'Publica'] = pem_public  
-    data_frame4.to_excel('./Autoridades.xlsx', index=False)"""
-
-# CÓDIGO PARA GENERAR EL CERTIFICADO AUTOFIRMADO DE LA ENTIDAD RAÍZ Y GENERAR LAS CSR
-
-"""for index, row in data_frame4.iterrows():
-    privada_autoridad = row["Privada"] 
-    autoridad_nombre = row["Autoridad"]
-    numero = 1
-    if autoridad_nombre == "Máxima":
-        privada_raiz = row["Privada"] 
-        subject = issuer = x509.Name([
-            x509.NameAttribute(NameOID.COUNTRY_NAME, "JP"),
-            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "East Blue"),
-            x509.NameAttribute(NameOID.LOCALITY_NAME, "La mar"),
-            x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Barba Negra Corp."),
-        ])
-        privada_autoridad_pem = serialization.load_pem_private_key(ast.literal_eval(privada_autoridad), password=None)
-        certificado_raiz = x509.CertificateBuilder().subject_name(
-            subject
-        ).issuer_name(
-            issuer
-        ).public_key(
-            privada_autoridad_pem.public_key()
-        ).serial_number(
-            x509.random_serial_number()
-        ).not_valid_before(
-            datetime.datetime.now(datetime.timezone.utc)
-        ).not_valid_after(
-            datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=10)
-        ).add_extension(
-            x509.SubjectAlternativeName([x509.DNSName("localhost")]),
-            critical=False,
-        ).sign(privada_autoridad_pem, hashes.SHA256())
-
-        data_frame4.at[index, 'Certificado'] = certificado_raiz
-        data_frame4.to_excel('./Autoridades.xlsx', index=False)
-    else:
-        privada_autoridad_pem = serialization.load_pem_private_key(ast.literal_eval(privada_autoridad), password=None)
-        csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
-            # Provide various details about who we are.
-            x509.NameAttribute(NameOID.COUNTRY_NAME, "JP"),
-            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "North Blue"),
-            x509.NameAttribute(NameOID.LOCALITY_NAME, "La mar del norte"),
-            x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Subordinada corp." + str(numero)),
-        ])).add_extension(
-            x509.SubjectAlternativeName([
-            ]),
-            critical=False,
-        # Sign the CSR with our private key.
-        ).sign(privada_autoridad_pem, hashes.SHA256())
-        numero += 1
-        csr_pem = csr.public_bytes(encoding=serialization.Encoding.PEM)
-
-        subordinada_certificado = x509.CertificateBuilder().subject_name(
-            csr.subject
-        ).issuer_name(
-            certificado_raiz.subject  
-        ).public_key(
-            csr.public_key()
-        ).serial_number(
-            x509.random_serial_number()
-        ).not_valid_before(
-            datetime.datetime.now(datetime.timezone.utc)
-        ).not_valid_after(
-            datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=365)
-        ).add_extension(
-            x509.BasicConstraints(ca=True, path_length=0), critical=True
-        ).sign(serialization.load_pem_private_key(ast.literal_eval(privada_raiz), password=None), hashes.SHA256(), default_backend())
-
-        # Serializar el certificado para almacenarlo o distribuirlo
-        subordinada_certificado_pem = subordinada_certificado.public_bytes(serialization.Encoding.PEM)
-        data_frame4.at[index, 'solicitud'] = csr_pem  
-        data_frame4.to_excel('./Autoridades.xlsx', index=False)
-        data_frame4.at[index, 'Certificado'] = subordinada_certificado_pem
-        data_frame4.to_excel('./Autoridades.xlsx', index=False)"""
-
-"""for index_usuario, row_usuario in data_frame3.iterrows():
-    privada_usuario = row_usuario["Privadas"] 
-    nombre_usuario = row_usuario["Nickname"]
-    autoridad_usuario = row_usuario["Entidad"]
-    for index_entidad, row_entidad in data_frame4.iterrows():
-        entidad = row_entidad["Autoridad"]
-        privada_autoridad = row_entidad["Privada"]
-        certificado_autoridad = row_entidad["Certificado"]
-        
-        if autoridad_usuario == 1 and entidad == "Subordinada_1":
-            print("all")
-            certificado_autoridad_pem = x509.load_pem_x509_certificate(ast.literal_eval(certificado_autoridad), default_backend())
-            privada_usuario_pem = serialization.load_pem_private_key(ast.literal_eval(privada_usuario), password=None)
-            subject_usuario = x509.Name([
-                x509.NameAttribute(NameOID.COUNTRY_NAME, "KR"),
-                x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Thousand Sunny"),
-                x509.NameAttribute(NameOID.LOCALITY_NAME, "La mar"),
-                x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Piratas anónimos"),
-                x509.NameAttribute(NameOID.COMMON_NAME, nombre_usuario),
-            ])
-            csr_usuario = x509.CertificateSigningRequestBuilder().subject_name(
-                subject_usuario
-            ).add_extension(
-                x509.SubjectAlternativeName([x509.DNSName("localhost")]),
-                critical=False,
-            ).sign(privada_usuario_pem, hashes.SHA256(), default_backend())
-
-            data_frame.at[index_usuario, 'Solicitud'] = csr_usuario.public_bytes(serialization.Encoding.PEM)
-            data_frame.to_excel('./datos_cripto.xlsx', index=False)
-            
-            certificado_usuario = x509.CertificateBuilder().subject_name(
-                subject_usuario
-            ).issuer_name(
-                certificado_autoridad_pem.subject  # El usuario es su propio emisor en este ejemplo
-            ).public_key(
-                csr_usuario.public_key()
-            ).serial_number(
-                x509.random_serial_number()
-            ).not_valid_before(
-                datetime.datetime.now(datetime.timezone.utc)
-            ).not_valid_after(
-                datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=365)
-            ).add_extension(
-                x509.SubjectAlternativeName([x509.DNSName("localhost")]),
-                critical=False,
-            ).sign(serialization.load_pem_private_key(ast.literal_eval(privada_autoridad), password=None), hashes.SHA256())
-
-            data_frame.at[index_usuario, 'Certificado'] = certificado_usuario.public_bytes(serialization.Encoding.PEM)
-            data_frame.to_excel('./datos_cripto.xlsx', index=False)
-        
-        elif autoridad_usuario == 2 and entidad == "Subordinada_2":
-            certificado_autoridad_pem = x509.load_pem_x509_certificate(ast.literal_eval(certificado_autoridad), default_backend())
-            privada_usuario_pem = serialization.load_pem_private_key(ast.literal_eval(privada_usuario), password=None)
-            subject_usuario = x509.Name([
-                x509.NameAttribute(NameOID.COUNTRY_NAME, "KR"),
-                x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Thousand Sunny"),
-                x509.NameAttribute(NameOID.LOCALITY_NAME, "La mar"),
-                x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Piratas anónimos"),
-                x509.NameAttribute(NameOID.COMMON_NAME, nombre_usuario),
-            ])
-            csr_usuario = x509.CertificateSigningRequestBuilder().subject_name(
-                subject_usuario
-            ).add_extension(
-                x509.SubjectAlternativeName([x509.DNSName("localhost")]),
-                critical=False,
-            ).sign(privada_usuario_pem, hashes.SHA256(), default_backend())
-
-            data_frame.at[index_usuario, 'Solicitud'] = csr_usuario.public_bytes(serialization.Encoding.PEM)
-            data_frame.to_excel('./datos_cripto.xlsx', index=False)
-            
-            certificado_usuario = x509.CertificateBuilder().subject_name(
-                subject_usuario
-            ).issuer_name(
-                certificado_autoridad_pem.subject  # El usuario es su propio emisor en este ejemplo
-            ).public_key(
-                csr_usuario.public_key()
-            ).serial_number(
-                x509.random_serial_number()
-            ).not_valid_before(
-                datetime.datetime.now(datetime.timezone.utc)
-            ).not_valid_after(
-                datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=365)
-            ).add_extension(
-                x509.SubjectAlternativeName([x509.DNSName("localhost")]),
-                critical=False,
-            ).sign(serialization.load_pem_private_key(ast.literal_eval(privada_autoridad), password=None), hashes.SHA256())
-
-            data_frame.at[index_usuario, 'Certificado'] = certificado_usuario.public_bytes(serialization.Encoding.PEM)
-            data_frame.to_excel('./datos_cripto.xlsx', index=False)"""
+data_frame5 = pd.read_excel("./Claves_publicas_autoridades.xlsx")
 
 
-#cada lista representa una fila en la pantalla de la app
-layout = [
-    [sg.Text('Por favor rellene con sus datos:')],
-    [sg.Text('Nickname', size=(15, 1)), sg.InputText(key='Nickname')],
-    [sg.Text('Contraseña', size=(15, 1)), sg.InputText(key='Contraseña', password_char='•')],
-    [sg.Submit('Aceptar'), sg.Exit('Salir')]
-]
 
-window = sg.Window('App Cripto', layout)
-
+def verificar_certificado(certificado, clave_publica_sup):
+    try:
+        clave_publica_sup.verify(
+            certificado.signature,
+            certificado.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            certificado.signature_hash_algorithm,
+        )
+        # print("La firma del certificado es válida.")     
+    except InvalidSignature:
+        # print("La firma del certificado no es válida.")
+        return False
+    return True
 #función para vaciar el input
 def clear_input():
     for key in values:
@@ -293,6 +52,16 @@ def validar_coordenadas(coordenadas):
         return True
     else:
         return False
+
+#cada lista representa una fila en la pantalla de la app
+layout = [
+    [sg.Text('Por favor rellene con sus datos:')],
+    [sg.Text('Nickname', size=(15, 1)), sg.InputText(key='Nickname')],
+    [sg.Text('Contraseña', size=(15, 1)), sg.InputText(key='Contraseña', password_char='•')],
+    [sg.Submit('Aceptar'), sg.Exit('Salir')]
+]
+
+window = sg.Window('App Cripto', layout)
 
 while True:
     event, values = window.read()   
@@ -325,7 +94,6 @@ while True:
                     p=1,
                 )              
                 key2 = kdf.derive(contraseña_bytes)
-
                 #si coinciden
                 if row['Nickname'] == nickname and key == key2:               
                     exito = True
@@ -449,9 +217,11 @@ while True:
                                 coordenadas_cifradas = row['Coordenadas']
                                 emisor = row['Emisor']
                                 firma = row['Firma']
+
                         for index, row in data_frame.iterrows():
                             if emisor == row['Nickname']:
                                 emisor_public = row['Key_public']
+                                emisor_certificado = row["Certificado"]
                         
                         if pd.isna(coordenadas_cifradas):
                             sg.popup("Todavía no se te han enviado coordenadas")
@@ -481,8 +251,6 @@ while True:
                             cadena_str = coordenadas_str[2:-1]
                             
                             try: 
-                                print("Intentando verificar la firma...")
-                                
                                 verification_result = emisor_public_pem.verify(
                                     ast.literal_eval(firma),
                                     coordenada_descifrada,
@@ -498,7 +266,44 @@ while True:
                                     print("Error al verificar la firma: La verificación no devolvió 'None'")
                             except Exception as e:
                                 print("Error al verificar la firma:", str(e))
-                            
+
+                            cadena_valida = False
+                            for index_u, row_u in data_frame.iterrows():
+                                
+                                entidad = row_u["Entidad"]
+                                certificado_emisor = row_u["Certificado"]
+                                certificado_emisor_pem = x509.load_pem_x509_certificate(ast.literal_eval(certificado_emisor), default_backend())
+                                for index_e, row_e in data_frame5.iterrows():
+                                    
+                                    if row_e["Autoridad"] == "Máxima":
+                                        publica_maxima = row_e["Publica"]
+                                        publica_maxima_pem = serialization.load_pem_public_key(ast.literal_eval(publica_maxima))
+                                    if nickname == row_u["Nickname"] and entidad == 1 and row_e["Autoridad"] == "Subordinada_1":
+                                        cert_subordinada1 = row_e["Certificado"]
+                                        cert_subordinada1_pem = x509.load_pem_x509_certificate(ast.literal_eval(cert_subordinada1), default_backend())
+                                        publica_subordinada1 = row_e["Publica"]
+                                        publica_subordinada1_pem = serialization.load_pem_public_key(ast.literal_eval(publica_subordinada1))
+                                        if not verificar_certificado(certificado_emisor_pem, publica_subordinada1_pem):
+                                            print("La cadena de certificados no es válida.")
+                                        else:
+                                            if not verificar_certificado(cert_subordinada1_pem, publica_maxima_pem):
+                                                print("La cadena de certificados no es válida.")
+                                            else:
+                                                print("La cadena de certificados es válida.")
+                                                cadena_valida = True
+                                    elif nickname == row_u["Nickname"] and entidad == 2 and row_e["Autoridad"] == "Subordinada_2":
+                                        cert_subordinada1 = row_e["Certificado"]
+                                        cert_subordinada1_pem = x509.load_pem_x509_certificate(ast.literal_eval(cert_subordinada1), default_backend())
+                                        publica_subordinada1 = row_e["Publica"]
+                                        publica_subordinada1_pem = serialization.load_pem_public_key(ast.literal_eval(publica_subordinada1))
+                                        if not verificar_certificado(certificado_emisor_pem, publica_subordinada1_pem):
+                                            print("La cadena de certificados no es válida.")
+                                        else:
+                                            if not verificar_certificado(cert_subordinada1_pem, publica_maxima_pem):
+                                                print("La cadena de certificados no es válida.")
+                                            else:
+                                                print("La cadena de certificados es válida.")
+                                                
                             layout_recibir = [
                                 [sg.Text('Tus coordenadas son:' + cadena_str)],
                                 [sg.Submit('Aceptar'), sg.Exit('Cancelar')]
